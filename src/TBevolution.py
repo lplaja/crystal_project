b6b3503d68ed08afc4358e00aa0fb5aa8390b72d
Luis Plaja <plaja@usal.es>
2026-06-15 10:41:02 +0200

    test_TBcrystal and test TB_evolution added and tested

--- src/TBevolution.py
+++ src/TBevolution.py
@@ -39,19 +39,11 @@ def _t_nm(kx,ky,kz,h_Rnm, deltas, R):
     t=np.empty((n_orb,n_orb,n_k), dtype=np.complex128)
     for n in range(n_orb):
         for m in range(n_orb):
-            #print(f'====    {n,m=}')
             tnm_vector=h_Rnm[:,n,m]
-            # arg_exp=np.array([1j*2*np.pi*kx*R[n,m,i_neighbor,0]+1j*2*np.pi*ky*R[n,m,i_neighbor,1]+1j*2*np.pi*kz*R[n,m,i_neighbor,2]
-            #                   for i_neighbor in range(R.shape[2])]).T # 1j*2*np.pi*kx*R[n,m,i_neighbor,0]+1j*2*np.pi*ky*R[n,m,i_neighbor,1] for u
-
             arg_exp = np.zeros((n_R, n_k), dtype=np.complex128)
             
             for iR in range(n_R):
-                #print(f'\t {iR=}')
                 R_delta=R[iR]+deltas[m]-deltas[n]
-                #print(f'\t  {R[iR]=}   {deltas[m]=}  {deltas[n]=}   {R_delta=} {tnm_vector[iR]/eV=}')
-                #print(f'\t  {kx=}   {ky=}   {kz=}')
-
                 arg_exp[iR, :] = (
                     1j * 2 * np.pi * kx * R_delta[0] +
                     1j * 2 * np.pi * ky * R_delta[1] +
@@ -62,10 +54,43 @@ def _t_nm(kx,ky,kz,h_Rnm, deltas, R):
             t[n,m]=np.dot(exp.T, tnm_vector)
     return t
 
+def _r_nm(kx,ky,kz,r_Rnm, R):
+    n_orb=r_Rnm.shape[1]
+    n_k=len(kx)
+    n_R = R.shape[0]
 
-@njit(parallel=False)
-def _rk_evolveCMCP(CM, CP, kx,ky, kz, Ax, Ay, Az, dt,  h_Rnm, deltas, R, from_it:int, to_it:int):
-#def _rk_evolveCMCP(CM, CP, kx,ky, kz, Ax, Ay, Az, dt,  neighbor_hoppings, neighbor_positions, from_it:int, to_it:int, a):
+    r=np.empty((n_orb,n_orb,n_k,3), dtype=np.complex128)
+    for n in range(n_orb):
+        for m in range(n_orb):
+            rnm_x_vector=r_Rnm[:,n,m,0]
+            rnm_y_vector=r_Rnm[:,n,m,1]
+            rnm_z_vector=r_Rnm[:,n,m,2]
+            arg_exp= np.zeros((n_R, n_k), dtype=np.complex128)
+            
+            for iR in range(n_R):
+                R_delta=R[iR]
+                arg_exp[iR, :] = (
+                    1j * 2 * np.pi * kx * R_delta[0] +
+                    1j * 2 * np.pi * ky * R_delta[1] +
+                    1j * 2 * np.pi * kz * R_delta[2]
+                )
+
+            exp=np.exp(arg_exp)
+            r[n,m,:,0]=np.dot(exp.T, rnm_x_vector)
+            r[n,m,:,1]=np.dot(exp.T, rnm_y_vector)
+            r[n,m,:,2]=np.dot(exp.T, rnm_z_vector)
+    return r
+
+def _eigen_hermitian(t_nm):
+    t_nm = np.moveaxis(t_nm, -1, 0)          # (N_k, N_orb, N_orb)
+    energies, eigvecs = np.linalg.eigh(t_nm)
+    # energies: (N_k, N_orb), eigvecs: (N_k, N_orb, N_orb)
+    energies = np.moveaxis(energies, 0, -1)      # (N_orb, N_k)
+    eigvecs = np.moveaxis(eigvecs, 0, -1)        # (N_orb, N_orb, N_k)
+    return energies, eigvecs
+
+@njit(parallel=True)
+def _rk_evolveCB(CB, kx,ky, kz, Ax, Ay, Az, Ex, Ey, Ez, dt,  h_Rnm, r_Rnm, deltas, R, from_it:int, to_it:int):
 
     itmax=len(Ax)
 
@@ -73,7 +98,6 @@ def _rk_evolveCMCP(CM, CP, kx,ky, kz, Ax, Ay, Az, dt,  h_Rnm, deltas, R, from_it
 
     c_qe__hbar=qe/hbar
     c1=-dt*1j/2/hbar
-
 
     n_orb=h_Rnm.shape[1]
     n_k=len(kx)
@@ -84,74 +108,69 @@ def _rk_evolveCMCP(CM, CP, kx,ky, kz, Ax, Ay, Az, dt,  h_Rnm, deltas, R, from_it
         norm_At_x=c_qe__hbar*Ax[it]
         norm_At_y=c_qe__hbar*Ay[it]
         norm_At_z=c_qe__hbar*Az[it]
+        qFt_x=qe*Ex
+        qFt_y=qe*Ey
+        qFt_z=qe*Ez
         if it==itmax:
             norm_Atdt_x=c_qe__hbar*(2*Ax[itmax]-Ax[itmax-1]) # extrapolation
             norm_Atdt_y=c_qe__hbar*(2*Ay[itmax]-Ay[itmax-1]) # extrapolation
             norm_Atdt_z=c_qe__hbar*(2*Az[itmax]-Az[itmax-1]) # extrapolation
+            qFtdt_x=qe*(2*Ex[itmax]-Ex[itmax-1]) # extrapolation
+            qFtdt_y=qe*(2*Ey[itmax]-Ey[itmax-1]) # extrapolation
+            qFtdt_z=qe*(2*Ez[itmax]-Ez[itmax-1]) # extrapolation
         else:
             norm_Atdt_x=c_qe__hbar*Ax[it+1]
             norm_Atdt_y=c_qe__hbar*Ay[it+1]
             norm_Atdt_z=c_qe__hbar*Az[it+1]
+            qFtdt_x=qe*Ex[it+1]
+            qFtdt_y=qe*Ey[it+1]
+            qFtdt_z=qe*Ez[it+1]
             
         norm_Atdt2_x=(norm_Atdt_x+norm_At_x)/2
         norm_Atdt2_y=(norm_Atdt_y+norm_At_y)/2
         norm_Atdt2_z=(norm_Atdt_z+norm_At_z)/2
+
+        qFtdt2_x=(qFtdt_x+qFt_x)/2
+        qFtdt2_y=(qFtdt_y+qFt_y)/2
+        qFtdt2_z=(qFtdt_z+qFt_z)/2
 
         if it==from_it:
             ktx=kx-norm_At_x
             kty=ky-norm_At_y
             ktz=kz-norm_At_z
             tnm=_t_nm(ktx,kty,ktz, h_Rnm, deltas, R)
+            rnm=_r_nm(ktx,kty,ktz, r_Rnm, R)
             ktx=kx-norm_Atdt_x
             kty=ky-norm_Atdt_y
             ktz=kz-norm_Atdt_z
             tnm_dt=_t_nm(ktx,kty,ktz, h_Rnm, deltas, R)
+            rnm_dt=_r_nm(ktx,kty,ktz, r_Rnm, R)
             tnm_dt2=(tnm_dt+tnm)/2
+            rnm_dt2=(rnm_dt+rnm)/2
         else:
-            ktx=kx-norm_At_x
-            kty=ky-norm_At_y
-            ktz=kz-norm_At_z
             tnm=tnm_dt
+            rnm=rnm_dt
             ktx=kx-norm_Atdt_x
             kty=ky-norm_Atdt_y
             ktz=kz-norm_Atdt_z
             tnm_dt=_t_nm(ktx,kty,ktz, h_Rnm, deltas, R)
+            rnm_dt=_r_nm(ktx,kty,ktz, r_Rnm, R)
             tnm_dt2=(tnm_dt+tnm)/2        
+            rnm_dt2=(rnm_dt+rnm)/2
 
-        # ktx=kx-norm_At_x
-        # kty=ky-norm_At_y
-        # ktz=kz-norm_At_z
-        # tnm=_t_nm(ktx,kty,ktz, h_Rnm, deltas, R)
-        sumE=np.real(tnm[0,0]+tnm[1,1])
-        diffE=2*tnm[0,1]
-        K1M=c1*(sumE*CM+diffE*CP)
-        K1P=c1*(sumE*CP+np.conjugate(diffE)*CM)
+        Mnm=tnm-qFt_x*rnm[:,0]-qFt_y*rnm[:,1]-qFt_z*rnm[:,2]
+        K1=c1*(np.einsum('nmi,mi->ni', Mnm, CB, optimize=True))
 
-        # ktx=kx-norm_Atdt2_x
-        # kty=ky-norm_Atdt2_y
-        # ktz=kz-norm_Atdt2_z
-        # tnm=_t_nm(ktx,kty,ktz, h_Rnm, deltas, R)
-        sumE=np.real(tnm_dt2[0,0]+tnm_dt2[1,1])
-        diffE=2*tnm_dt2[0,1]
-        K2M=c1*(sumE*(CM+K1M/2)+diffE*(CP+K1P/2))
-        K2P=c1*(sumE*(CP+K1P/2)+np.conjugate(diffE)*(CM+K1M/2))
-                
-        K3M=c1*(sumE*(CM+K2M/2)+diffE*(CP+K2P/2))
-        K3P=c1*(sumE*(CP+K2P/2)+np.conjugate(diffE)*(CM+K2M/2))
+        Mnm=tnm_dt2-qFtdt2_x*rnm_dt2[:,0]-qFtdt2_y*rnm_dt2[:,1]-qFtdt2_z*rnm_dt2[:,2]
+        K2=c1*(np.einsum('nmi,mi->ni', Mnm, CB+K1/2, optimize=True))
+        K3=c1*(np.einsum('nmi,mi->ni', Mnm, CB+K2/2, optimize=True))
+  
+        Mnm=tnm_dt-qFtdt_x*rnm_dt[:,0]-qFtdt_y*rnm_dt[:,1]-qFtdt_z*rnm_dt[:,2]
+        K4=c1*(np.einsum('nmi,mi->ni', Mnm, CB+K3, optimize=True))
+  
+        CB+=K1/6+K2/3+K3/3+K4/6
 
-        # ktx=kx-norm_Atdt_x
-        # kty=ky-norm_Atdt_y
-        # ktz=kz-norm_Atdt_z
-        # tnm=_t_nm(ktx,kty,ktz, h_Rnm, deltas, R)
-        sumE=np.real(tnm_dt[0,0]+tnm_dt[1,1])
-        diffE=2*tnm_dt[0,1]
-        K4M=c1*(sumE*(CM+K3M)+diffE*(CP+K3P))
-        K4P=c1*(sumE*(CP+K3P)+np.conjugate(diffE)*(CM+K3M))
-
-        CM+=K1M/6+K2M/3+K3M/3+K4M/6
-        CP+=K1P/6+K2P/3+K3P/3+K4P/6
-
-    return CM, CP
+    return CB
 
 class TBevolution_CMCP:
 
@@ -315,4 +334,178 @@ class TBevolution_CMCP:
             dipole_y[min(it//istep+1, len(dipole_x)-1)]=1j*qe/2*np.sum((np.conj(CMw)*gradCM_y+np.conj(CPw)*gradCP_y)*self.dV)
         
         return time_dip, dipole_x, dipole_y
-            
+
+class TBevolution_Bloch:
+
+    def __init__(self, crystal:cr.crystal, Field:PolarizedHarmonicField):
+
+        if not crystal.grid.cartesian:
+            raise ValueError("crystal.grid must be cartesian")
+
+        self.crystal = crystal
+
+        self.Field=Field
+
+        self.k=self.crystal.grid.x[self.crystal.grid.inGrid] # note that k are points from the filtered grid
+        self.dV=self.crystal.grid.dV[self.crystal.grid.inGrid]
+        self.V=np.sum(self.dV)
+
+        if self.crystal.grid.ndim == 2:
+            if Field.s_direction[0]!= 0 or Field.s_direction[1]!= 0:
+                raise ValueError("s_direction must be [0,0,1], orthogonal to the xy plane")
+            # Añadir columna de ceros para kz
+            kz = np.zeros((self.k.shape[0], 1), dtype=np.float64)
+            self.k = np.column_stack([self.k, kz])
+        elif self.crystal.grid.ndim == 1:
+            if Field.s_direction[0]!= 0:
+                raise ValueError("s_direction must be orthogonal to x")
+            # Añadir columnas de ceros para kz y ky
+            kz = np.zeros((self.k.shape[0], 1), dtype=np.float64)
+            ky = np.zeros((self.k.shape[0], 1), dtype=np.float64)
+            self.k = np.column_stack([self.k, ky, kz])    
+
+        self.h_Rnm = self.crystal.h_Rnm * eV / self.crystal.deg_weights[:, None, None] # convert to eV and apply degeneracy weights
+        self.r_Rnm=self.crystal.r_Rnm
+
+        self.R=cr.vector_in_cart(self.crystal.R_vectors, self.crystal.direct_vectors) # coordinate of the WZ cell in cartesian
+
+        self.deltas=self.crystal.deltas
+        self.num_wann=self.crystal.num_wann
+        self.nrpts=self.crystal.nrpts
+
+        kx=self.k[:,0]
+        ky=self.k[:,1]
+        kz=self.k[:,2]
+
+        # set the initial amplitudes of the Bloch states. 
+        # As all the population is in the lower band, the Bloch state aplitudes correspond to the 
+        # lower band amplitudes. CB has the coeficients for the orbitals in each column
+
+        _,CB=self.bands(kx,ky,kz)
+        self.CB=CB
+        _,CB=self.bands(kx+epsk,ky,kz)
+        self.CB_px=CB
+        _,CB=self.bands(kx-epsk,ky,kz)
+        self.CB_mx=CB
+        _,CB=self.bands(kx,ky+epsk,kz)
+        self.CB_py=CB
+        _,CB=self.bands(kx,ky-epsk,kz)
+        self.CB_my=CB
+
+    def __repr__(self):
+        info=f"# {self.__class__.__name__}:  id= {id(self):x} \n"
+        info+=f"# \n"
+        return info
+    
+    def bands(self,kx,ky,kz):
+        return _eigen_hermitian(self.tnm(kx,ky,kz))
+    
+    def tnm(self,kx,ky,kz):
+        return _t_nm(kx, ky, kz, self.h_Rnm, self.deltas, self.R)
+    
+    def rnm(self,kx,ky,kz):
+        return _r_nm(kx, ky, kz, self.r_Rnm, self.R)
+    
+    def grad_phik_x(self,kx,ky,kz):
+        tnm=self.tnm(kx-epsk,ky,kz)
+        phi_k_m=np.angle(tnm[0,1])
+        tnm=self.tnm(kx+epsk,ky,kz)
+        phi_k_p=np.angle(tnm[0,1])
+        return (phi_k_p-phi_k_m)/(2*epsk)/self.crystal.reciprocal_lattice_unit
+    
+    def grad_phik_y(self,kx,ky,kz):
+        
+        tnm=self.tnm(kx,ky-epsk,kz)
+        phi_k_m=np.angle(tnm[0,1])
+        tnm=self.tnm(kx,ky+epsk,kz)
+        phi_k_p=np.angle(tnm[0,1])
+        return (phi_k_p-phi_k_m)/(2*epsk)/self.crystal.reciprocal_lattice_unit
+        
+    def rk_evolve(self,CB, kx, ky, kz, from_it:int, to_it:int):
+        if to_it>len(self.Field.A[:,0])-1:
+            to_it=len(self.Field.A[:,0])-1
+
+        # Assume that the parallel direction is in the plane x,z and perp in the direction y
+        Ax=self.Field.A[:,0]/self.crystal.reciprocal_lattice_unit*self.Field.s_direction[2]
+        Ex=self.Field.E[:,0]/self.crystal.reciprocal_lattice_unit*self.Field.s_direction[2]
+        sinus=np.sqrt(1-self.Field.s_direction[2]**2)
+        Az=self.Field.A[:,0]/self.crystal.reciprocal_lattice_unit*sinus
+        Ez=self.Field.E[:,0]/self.crystal.reciprocal_lattice_unit*sinus
+
+        ## add the axial part
+        Ax+=self.Field.A[:,2]/self.crystal.reciprocal_lattice_unit*sinus
+        Ex+=self.Field.E[:,2]/self.crystal.reciprocal_lattice_unit*sinus
+        Az+=self.Field.A[:,2]/self.crystal.reciprocal_lattice_unit*self.Field.s_direction[2]
+        Ez+=self.Field.E[:,2]/self.crystal.reciprocal_lattice_unit*self.Field.s_direction[2]
+    
+        Ay=self.Field.A[:,1]/self.crystal.reciprocal_lattice_unit
+        Ey=self.Field.E[:,1]/self.crystal.reciprocal_lattice_unit
+
+        CB=_rk_evolveCB(CB, kx,ky,kz, Ax, Ay, Az, Ex, Ey, Ez, self.Field.dt, self.h_Rnm, self.r_Rnm, self.deltas, self.R, from_it, to_it)
+
+        return CB
+    
+    def rk_dipole(self, npt:int):
+        
+        start_time = time.time()
+
+        imax=len(self.Field.t)+1
+        istep=imax//npt
+
+        Cvw=self.Cv.copy()
+        Ccw=self.Cc.copy()
+        Cv_px=self.Cv_px.copy()
+        Cc_px=self.Cc_px.copy()
+        Cv_mx=self.Cv_mx.copy()
+        Cc_mx=self.Cc_mx.copy()
+        Cv_py=self.Cv_py.copy()
+        Cc_py=self.Cc_py.copy()
+        Cv_my=self.Cv_my.copy()
+        Cc_my=self.Cc_my.copy()
+        dk=epsk*self.crystal.reciprocal_lattice_unit
+
+        time_dip=np.zeros(npt,dtype=np.float64)
+        dipole_x=np.zeros(npt,dtype=np.complex128)
+        dipole_y=np.zeros(npt,dtype=np.complex128)
+
+
+        gradCv_x=(Cv_px-Cv_mx)/(2*dk)
+        gradCc_x=(Cc_px-Cc_mx)/(2*dk)
+        gradCv_y=(Cv_py-Cv_my)/(2*dk)
+        gradCc_y=(Cc_py-Cc_my)/(2*dk)
+
+        dipole_x[0]=1j*qe/2*np.sum((np.conj(CMw)*gradCM_x+np.conj(CPw)*gradCP_x)*self.dV)
+        dipole_y[0]=1j*qe/2*np.sum((np.conj(CMw)*gradCM_y+np.conj(CPw)*gradCP_y)*self.dV)
+
+        c_qe__hbar=qe/hbar
+
+        kx=self.k[:,0]
+        ky=self.k[:,1]
+        kz=self.k[:,2]
+
+        for it in range(0,imax,istep):
+            if it+istep>=imax:
+                break
+            elapsed = time.time() - start_time
+            if it!=0:
+                remaining=elapsed/it*(imax-it)
+                formatted_elapsed = str(timedelta(seconds=elapsed))
+                formatted_remaining = str(timedelta(seconds=remaining))
+                logging.info(f"elap. time {formatted_elapsed} \t rem. time {formatted_remaining} step= {it//istep}/{imax//istep}")
+
+            CMw, CPw=self.rk_evolve(CMw, CPw, kx, ky, kz, it, it+istep)
+            CM_px, CP_px=self.rk_evolve(CM_px, CP_px, kx+epsk, ky, kz, it, it+istep)
+            CM_mx, CP_mx=self.rk_evolve(CM_mx, CP_mx, kx-epsk, ky, kz, it, it+istep)
+            CM_py, CP_py=self.rk_evolve(CM_py, CP_py, kx, ky+epsk, kz, it, it+istep)
+            CM_my, CP_my=self.rk_evolve(CM_my, CP_my, kx, ky-epsk, kz, it, it+istep)
+
+            gradCM_x=(CM_px-CM_mx)/(2*dk)
+            gradCP_x=(CP_px-CP_mx)/(2*dk)
+            gradCM_y=(CM_py-CM_my)/(2*dk)
+            gradCP_y=(CP_py-CP_my)/(2*dk)
+
+            time_dip[min(it//istep+1, len(dipole_x)-1)]=self.Field.t[it+istep,0]
+            dipole_x[min(it//istep+1, len(dipole_x)-1)]=1j*qe/2*np.sum((np.conj(CMw)*gradCM_x+np.conj(CPw)*gradCP_x)*self.dV)
+            dipole_y[min(it//istep+1, len(dipole_x)-1)]=1j*qe/2*np.sum((np.conj(CMw)*gradCM_y+np.conj(CPw)*gradCP_y)*self.dV)
+        
+        return time_dip, dipole_x, dipole_y      
---
+++ tests/test_TBevolution.py
@@ -0,0 +1,92 @@
+import sys
+sys.path.append("/home/lplaja/crystal_project/src")
+from scipy.constants import eV
+import numpy as np
+import TBcrystal as cr
+import grid as grid
+import Field as Field
+import TBevolution as TBevolution
+
+
+def test_TBevolution_Bloch_parameters():
+        # Create an hexagonal grid
+        filter=grid.polygonfilter
+        filter_args={'nsides':6, 'radius': 2/3}
+        ggr=grid.UniformCartesianGrid(2,[[-0.75,0.75],[-0.75,0.75]],[500,500], origin=(0.0,0), filter=filter, filter_args= filter_args)
+
+        # Create the graphene crystal
+
+        TBcr=cr.crystal.from_W90_TB_file(filename='/home/lplaja/crystal_project/calculations/Wannier90 data/gr1NN_tb.dat' , 
+                                                                        grid=ggr, species_name='graphene', threshold_hopping=0)
+        
+        lambda0=3000*1e-9
+        T0=Field.lambda2T(lambda0)
+        tini=0*T0
+        tfin=8*T0
+        limits=[tini,tfin]
+        nptx=32768
+        tt=grid.UniformCartesianGrid(1,limits=limits, nptx=nptx)
+
+        fieldcallable=Field.polarizedHarmonicElectricField        
+        I_W__cm2=5e10
+        env_params={'start' : 0, 'end' : 1, 'ton'   : 0.5, 'toff'  : 0.5 } # in units of the time grid
+        phi_rad=-1.5707963267948966
+        chi_rad=1.5707963267948966
+        ellip=0
+        Efield=fieldcallable(tt,I_W__cm2=I_W__cm2,lambda0_nm=lambda0*1e9, env=Field.env_sin2, env_parameters=env_params, phi_rad=phi_rad, chi_rad=chi_rad,ellip=ellip)
+
+        TBev=TBevolution.TBevolution_Bloch(TBcr, Efield)
+        
+
+        # self.h_Rnm = self.crystal.h_Rnm * eV / self.crystal.deg_weights[:, None, None]
+        assert np.allclose(TBev.h_Rnm[0,:,:], np.array([[0. + 0.j, -2.8879914 + 0.j], [0. + 0.j, 0. + 0.j]])*eV)
+        assert np.allclose(TBev.h_Rnm[2,:,:], np.array([[0. + 0.j, -2.8879914 + 0.j], [ -2.8879914 + 0.j, 0. + 0.j]])*eV)
+
+        # self.R=cr.vector_in_cart(self.crystal.R_vectors, self.crystal.direct_vectors) # coordinate of the WZ cell in cartesian
+        assert np.allclose(TBev.R[0], np.array([-2.1304224930000002, -1.2300000000000000,0]))
+        assert np.allclose(TBev.R[1], np.array([-2.1304224930000002, 1.2300000000000000,0]))
+        assert np.allclose(TBev.R[2], np.array([0, 0,0]))
+        assert np.allclose(TBev.R[3], np.array([2.1304224930000002, -1.2300000000000000,0]))
+        assert np.allclose(TBev.R[4], np.array([2.1304224930000002, 1.2300000000000000,0]))
+
+
+
+def test_TBevolution_Bloch_bands():
+        # Create an hexagonal grid
+        filter=grid.polygonfilter
+        filter_args={'nsides':6, 'radius': 2/3}
+        ggr=grid.UniformCartesianGrid(2,[[-0.75,0.75],[-0.75,0.75]],[500,500], origin=(0.0,0), filter=filter, filter_args= filter_args)
+
+        # Create the graphene crystal
+
+        TBcr=cr.crystal.from_W90_TB_file(filename='/home/lplaja/crystal_project/calculations/Wannier90 data/gr1NN_tb.dat' , 
+                                                                        grid=ggr, species_name='graphene', threshold_hopping=0)
+        
+        lambda0=3000*1e-9
+        T0=Field.lambda2T(lambda0)
+        tini=0*T0
+        tfin=8*T0
+        limits=[tini,tfin]
+        nptx=32768
+        tt=grid.UniformCartesianGrid(1,limits=limits, nptx=nptx)
+
+        fieldcallable=Field.polarizedHarmonicElectricField        
+        I_W__cm2=5e10
+        env_params={'start' : 0, 'end' : 1, 'ton'   : 0.5, 'toff'  : 0.5 } # in units of the time grid
+        phi_rad=-1.5707963267948966
+        chi_rad=1.5707963267948966
+        ellip=0
+        Efield=fieldcallable(tt,I_W__cm2=I_W__cm2,lambda0_nm=lambda0*1e9, env=Field.env_sin2, env_parameters=env_params, phi_rad=phi_rad, chi_rad=chi_rad,ellip=ellip)
+
+        TBev=TBevolution.TBevolution_Bloch(TBcr, Efield)
+
+        a=1.42
+        kx=np.array([0,2/3/a*np.sqrt(3)/2, 2/3/a*np.sqrt(3)/2] )
+        ky=np.array([0,2/3/a*1/2,0])
+        kz=np.array([0,0,0])
+        en, eig=TBev.bands(kx,ky,kz)
+
+        assert np.allclose(en[:,0], np.array([-8.6639742, 8.6639742])*eV)  # Gamma
+        assert np.allclose(en[:,1], np.array([0, 0])*eV)  # K
+        assert np.allclose(en[:,2], np.array([-2.888, 2.888])*eV)  # M
+
